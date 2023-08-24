692f3ed2802f51229fc69b2545f96e153f13372c
joefavergel <joseph@guane.com.co>
2023-08-24 00:26:01 -0500

    :construction: Add GUI initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use raylib_ffi::{InitWindow, CloseWindow, WindowShouldClose, BeginDrawing, EndDrawing, DrawPixel, Color};
+
+fn init_gui() {
+    unsafe {
+        InitWindow(400, 400, "Fire".as_ptr() as *const i8)
+    }
+}
+
+fn window_should_close() -> bool {
+    unsafe { WindowShouldClose() }
+}
+
+fn begin_drawing() {
+    unsafe {
+        BeginDrawing()
+    }
+}
+
+fn end_drawing() {
+    unsafe {
+        EndDrawing()
+    }
+}
+
+fn close_window() {
+    unsafe {
+        CloseWindow()
+    }
+}
+
+fn draw_pixel(x: i32, y: i32, color: Color) {
+    unsafe {
+        DrawPixel(x, y, color)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    init_gui();
+
+    while ! window_should_close() {
+        begin_drawing();
+        draw_pixel(200, 200, Color{r:0xFF, g:0, b: 0, a:0xFF});
+        end_drawing()
+    }
+
+    close_window();
 }
