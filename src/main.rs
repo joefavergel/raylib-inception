a30ca6be7435931afff4acd40451b69cf16020c7
joefavergel <joseph@guane.com.co>
2023-08-26 19:10:18 -0500

    :sparkles: Add embers effect to fire

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use raylib_ffi::{
     Texture2D, UpdateTexture
 };
 use std::ffi::c_void;
+use tinyrand::{StdRand, Wyrand, RandRange};
 
 fn main() {
     init_gui();
@@ -27,10 +28,13 @@ fn main() {
         format: enums::PixelFormat::R8g8b8a8 as i32
     };
     let screen_buffer_texture = load_texture_from_image(screen_buffer);
+    let palette = generate_palette();
+    let mut fire_buffer = [ 0u8; 400 * 300 ];
+    let mut rng = StdRand::default();
 
     while ! window_should_close() {
         begin_drawing();
-        draw_next_frame(&mut screen_buffer_data);
+        draw_next_frame(&mut screen_buffer_data, &mut fire_buffer, &palette, &mut rng);
         update_texture(screen_buffer_texture, &screen_buffer_data);
         // draw_pixel(200, 200, Color{r:0xFF, g:0, b: 0, a:0xFF});
         draw_texture(screen_buffer_texture, 0, 0, WHITE);
@@ -42,7 +46,7 @@ fn main() {
 
 fn init_gui() {
     unsafe {
-        InitWindow(400, 400, "Fire".as_ptr() as *const i8)
+        InitWindow(400, 300, "Fire".as_ptr() as *const i8)
     }
 }
 
@@ -92,8 +96,65 @@ fn update_texture(tex: Texture2D, data: &[Color]) {
     }
 }
 
-fn draw_next_frame(screen:  &mut [Color]){
-    let pixel = &mut screen[200*400 + 200];
-    *pixel = Color{r:0xFF,g:0,b:0,a:0xFF};
+fn draw_next_frame(screen:  &mut [Color], fire_buf: &mut [u8], pal: &[Color], rng: &mut Wyrand) {
+    // Draw a pixel at middle
+    // let pixel = &mut screen[200*400 + 200];
+    // *pixel = Color{r:0xFF,g:0,b:0,a:0xFF};
 
+    draw_palette(screen, pal);
+    fill_bottom_with_random_ashes(fire_buf, rng);
+    convert_fire_buffer_to_screen(fire_buf, pal, screen);
+
+}
+
+fn generate_palette() -> [Color; 256] {
+    let mut pal: [Color; 256] = [ Color{r:0,g:0,b:0,a:0xFF} ; 256];
+
+    for i in 0..=84 {
+        pal[i].r = (i * (0xFF / 85)) as u8;
+    }
+
+    for i in 85..=169 {
+        pal[i].r = 255;
+        pal[i].g = ((i - 85) * (0xFF / 85)) as u8;
+    }
+
+    for i in 170..=255 {
+        pal[i].r = 255;
+        pal[i].g = 255;
+        pal[i].b = ((i - 170) * (0xFF / 85)) as u8;
+    }
+
+    pal
+}
+
+fn draw_palette(screen:  &mut [Color], pal: &[Color]) {
+    for i in 0..pal.len() {
+        let init = i * 400 + 50;
+        let pixels = &mut screen[ init..(init + 4) ];
+        // for i in 0..400 {
+        //     pixels[i] = pal[i]
+        // }
+        pixels[0] = pal[i];
+        pixels[1] = pal[i];
+        pixels[2] = pal[i];
+        pixels[3] = pal[i];
+    }
+}
+
+fn fill_bottom_with_random_ashes(fire_buf: &mut [u8], rng: &mut Wyrand) {
+    let end = fire_buf.len();
+    let start = end - 400;
+    for i in start..end {
+        fire_buf[i] = rng.next_range(0..256u16) as u8;
+    }
+
+
+}
+
+fn convert_fire_buffer_to_screen(fire_buf: &[u8], pal: &[Color], screen:  &mut [Color]) {
+    for i in 0..fire_buf.len() {
+        let heat = fire_buf[i] as usize;
+        screen[i] = pal[ heat ];
+    }
 }
